c77687b21fc8726ed9156944bb02f1903eb2078b
shahzeb-iftikhar <shahzebiftikharmalicc@gmail.com>
2025-05-11 22:55:14 +0200

    feat: create resources for teams table
    - Created migration and model for the teams table.
    - Generated controller.
    - Defined relationships in the teams model.
    - Created factory and seeder for populating the team table with dummy data.
    - Added basic validation for creating and updating team records.

---
+++ app/Http/Resources/TeamResource.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Resources;
+
+use App\Models\Team;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+/** @mixin Team */
+class TeamResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id'         => $this->id,
+            'name'       => $this->name,
+            'created_by' => [
+                'id'    => $this->creator->id,
+                'name'  => $this->creator->full_name ?? $this->creator->username,
+                'email' => $this->creator->email,
+            ],
+            'created_at' => $this->created_at->toDateTimeString(),
+            'updated_at' => $this->updated_at->toDateTimeString(),
+        ];
+    }
+}
---
+++ database/migrations/2025_05_11_203521_create_teams_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('teams', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name', 100);
+            $table->foreignId('created_by')->constrained('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('teams');
+    }
+};
